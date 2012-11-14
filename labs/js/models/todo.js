d9478d819c5d78944ac5141e38478aa512aa5f25
Nate Schutta <ntschutta@gmail.com>
2012-11-13 22:49:44 -0600

    added lab comments

--- labs/js/models/todo.js
+++ labs/js/models/todo.js
@@ -3,7 +3,19 @@ var app = app || {};
 (function() {
 	'use strict';
 
+	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
+	// you'll want to extend the Backbone model here
+	
+		// Default attributes for the todo
+		// define the default attributes
+
+		// Toggle the `completed` state of this todo item.
+		toggle: function() {
+			this.save({
+				completed: !this.get('completed')
+			});
+		}
 
 	});
-
+	
 }());
