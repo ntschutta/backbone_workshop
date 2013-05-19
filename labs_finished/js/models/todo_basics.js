2506dbf018ecab9cca57371219b309959f1d5940
Nate Schutta <ntschutta@gmail.com>
2013-05-19 11:31:07 -0500

    fixed extra comma issue

--- labs_finished/js/models/todo_basics.js
+++ labs_finished/js/models/todo_basics.js
@@ -13,7 +13,7 @@ var app = app || {};
 		// and ensure that each todo created has `title` and `completed` keys.
 		defaults: {
 			title: '',
-			completed: false,
+			completed: false
 		},
 
 		// Toggle the `completed` state of this todo item.
